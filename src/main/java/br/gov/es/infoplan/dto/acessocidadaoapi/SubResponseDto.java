4d765e646fc6f5a5d4591012ceff3a4a0f42945c
Gean Guimarães Carneiro <gean.carneiro@sep.local>
2024-10-31 17:00:19 -0300

    ajustar arvore de arquivos

--- src/main/java/br/gov/es/infoplan/dto/acessocidadaoapi/SubResponseDto.java
+++
@@ -1,5 +0,0 @@
-package br.gov.es.infoplan.dto.acessocidadaoapi;
-
-public record SubResponseDto(
-        String sub) {
-}
--- src/main/java/br/gov/es/infoplan/exception/UsuarioSemPermissaoException.java
+++
@@ -1,8 +0,0 @@
-package br.gov.es.infoplan.exception;
-
-public class UsuarioSemPermissaoException extends RuntimeException {
-
-    public UsuarioSemPermissaoException() {
-        super("Usuário sem permissão.");
-    }
-}
